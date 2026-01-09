5a0169fa113cec9c25ed20c54d42fae3e45081cf
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-09 15:02:12 +0530

    Add files via upload

---
+++ Core Python/Assignment/Assignment 1(Basic)/que_2.py
@@ -0,0 +1,9 @@
+# WAP to calculate area of rectangle based on length and breadth
+
+length = float(input("Enter the length of rectangle: "))
+breadth = float(input("Enter the breadth of rectangle: "))
+ 
+ #Calculate area
+area = length * breadth
+
+print("Area of rectangle:", area)
