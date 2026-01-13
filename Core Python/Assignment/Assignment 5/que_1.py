0d6f9631cc04c6b380192011ea48c6411f5d00fb
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-13 22:25:23 +0530

    Update que_1.py

--- Core Python/Assignment/Assignment 5/que_1.py
+++ Core Python/Assignment/Assignment 5/que_1.py
@@ -1,1 +1,16 @@
+userid = input("Enter the UserID : ")
+Password = input("Enter the Password : ")
 
+c_userid = "rucha"
+c_pass = "12345"
+
+if userid == c_userid and Password == c_pass:
+        print("Login Successfully!")
+else:
+    print("You get 3 chances to enter correct credentials! ")
+    for i in range(1, 4):
+        if userid == c_userid and Password == c_pass:
+            print("Login Successfully!")
+        else:    
+            userid = input("Enter the UserID : ")
+            Password = input("Enter the Password : ")
