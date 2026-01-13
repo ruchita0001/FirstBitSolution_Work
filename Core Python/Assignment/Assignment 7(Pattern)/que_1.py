14577aa83d4dae48e3d3381b020c5e635a359198
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-13 22:52:11 +0530

    Add hollow hourglass star pattern generator
    This script generates a hollow hourglass star pattern using asterisks.

---
+++ Core Python/Assignment/Assignment 7(Pattern)/que_1.py
@@ -0,0 +1,28 @@
+# diamond / hollow hourglass star pattern
+    *
+   * *
+  *   *
+ *     *
+*       *
+*       *
+ *     *
+  *   *
+   * *
+    *
+
+n = 5  
+# Upper half
+for i in range(1, n+1):
+    print(" " * (n - i), end="")  # Leading spaces
+    if i == 1:
+        print("*")
+    else:
+        print("*" + " " * (2*i - 3) + "*")
+
+# Lower half
+for i in range(n, 0, -1):
+    print(" " * (n - i), end="")  # Leading spaces
+    if i == 1:
+        print("*")
+    else:
+        print("*" + " " * (2*i - 3) + "*")
