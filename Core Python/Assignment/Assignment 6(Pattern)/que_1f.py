79f12a7a93212c8445be8b40de372fdfacf89a94
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-13 22:39:43 +0530

    Implement pyramid pattern printing
    Added a pyramid pattern of numbers to the program.

--- Core Python/Assignment/Assignment 6(Pattern)/que_1f.py
+++ Core Python/Assignment/Assignment 6(Pattern)/que_1f.py
@@ -1,4 +1,10 @@
 # Program to print a pyramid pattern of numbers
+        1 
+      1 2 3 
+    1 2 3 4 5
+  1 2 3 4 5 6 7
+1 2 3 4 5 6 7 8 9
+
 
 n = 5   # number of rows
 for i in range(1, n+1):
@@ -9,5 +15,4 @@ for i in range(1, n+1):
     # printing numbers
     for num in range(1, 2*i):
         print(num, end=" ")
-
     print()
