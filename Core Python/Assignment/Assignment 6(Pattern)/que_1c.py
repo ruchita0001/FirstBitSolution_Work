8e25eda3ffd0d15c7f90413d0e438f7d04d89699
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-13 22:37:23 +0530

    Reformat Pascal's Triangle code for clarity

--- Core Python/Assignment/Assignment 6(Pattern)/que_1c.py
+++ Core Python/Assignment/Assignment 6(Pattern)/que_1c.py
@@ -1,7 +1,11 @@
 # Pascal’s Triangle
+   1 
+  1 1
+ 1 2 1
+1 3 3 1
+
 
 rows = 4
-
 for i in range(rows):
     print(" " * (rows - i - 1), end="")  # Leading spaces
     num = 1
@@ -9,3 +13,4 @@ for i in range(rows):
         print(num, end=" ")
         num = num * (i - j) // (j + 1)  # Next number in Pascal's Triangle
     print()
+
