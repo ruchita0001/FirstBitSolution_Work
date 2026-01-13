740d942e69936747029d6c151f6a5047ce533439
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-13 22:40:36 +0530

    Fix formatting of alphabet pyramid pattern output

--- Core Python/Assignment/Assignment 6(Pattern)/que_1g.py
+++ Core Python/Assignment/Assignment 6(Pattern)/que_1g.py
@@ -1,7 +1,12 @@
 # Alphabet pyramid pattern
+        A 
+      A B C
+    A B C D E
+  A B C D E F G
+A B C D E F G H I
+
 
 n = 5   # number of rows
-
 for i in range(1, n+1):
 
     # printing spaces
@@ -13,3 +18,4 @@ for i in range(1, n+1):
         print(chr(65 + alpha - 1), end=" ")
 
     print()
+
