4c487ecec20d5de93ad668e4c012a43fc5eea324
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-13 22:58:26 +0530

    Add mirrored number pyramid pattern in que_4.py
    Implement number pyramid pattern with mirrored numbers.

---
+++ Core Python/Assignment/Assignment 7(Pattern)/que_4.py
@@ -0,0 +1,23 @@
+# number pyramid with mirrored numbers
+        1 
+      2 3 2 
+    3 4 5 4 3
+  4 5 6 7 6 5 4
+5 6 7 8 9 8 7 6 5
+
+
+n = 5  
+for i in range(1, n+1):
+    print(" " * (n - i) * 2, end="")
+
+    num = i
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+      
+    num -= 2
+    for j in range(i-1):
+        print(num, end=" ")
+        num -= 1
+
+    print()  
