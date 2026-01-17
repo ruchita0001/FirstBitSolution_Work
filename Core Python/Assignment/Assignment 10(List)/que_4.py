4c7126ebb3f32c25a602d027ad40063924ee8b60
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-17 23:34:15 +0530

    Add list reversal functionality in que_4.py
    Implement a program to reverse a list of integers entered by the user.

---
+++ Core Python/Assignment/Assignment 10(List)/que_4.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the no. of element : "))
+l = []
+for i in range(n):
+    l.append(int(input()))
+
+print("Original List : ",l)
+
+for i in range(n//2):
+    l[i], l[n-1-i] = l[n-1-i], l[i]
+
+print("Reverse List : ", l)
