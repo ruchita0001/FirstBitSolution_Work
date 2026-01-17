2e51edaccfb1aab04f90336af15ed715cf591ad0
Ruchita Dipak Gate <ruchitagate7@gmail.com>
2026-01-17 23:32:50 +0530

    Implement sum calculation for user-input elements

--- Core Python/Assignment/Assignment 10(List)/que_1.py
+++ Core Python/Assignment/Assignment 10(List)/que_1.py
@@ -1,1 +1,10 @@
-e
+n = int(input("Enter the no. of element : "))
+l = []
+for i in range(n):
+    l.append(int(input()))
+
+sum = 0
+for ele in l:
+    sum += ele
+
+print("Sum of elements : ", sum)
