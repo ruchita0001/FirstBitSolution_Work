42d95d278f58368116f86f003148a1654d756ebe
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-18 23:48:59 +0530

    Update que_2.py

--- Core Python/Assignment/Assignment 10(List)/que_2.py
+++ Core Python/Assignment/Assignment 10(List)/que_2.py
@@ -1,1 +1,15 @@
 # WAP to find maximum and minimum element in a list.
+
+lst = [12, 45, 7, 89, 23, 56]
+max_val = lst[0]
+min_val = lst[0]
+for i in lst:
+    if i > max_val:
+        max_val = i
+    if i < min_val:
+        min_val = i
+
+print("Maximum element is:", max_val)
+print("Minimum element is:", min_val)
+
+
