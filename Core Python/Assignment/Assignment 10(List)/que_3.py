2fc97dda7b9874a0461714de6487a166da7a6f7a
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-18 23:50:33 +0530

    Update que_3.py

--- Core Python/Assignment/Assignment 10(List)/que_3.py
+++ Core Python/Assignment/Assignment 10(List)/que_3.py
@@ -1,1 +1,14 @@
-q
+# WAP to find the second largest element in the list.
+lst = [12, 45, 7, 89, 23, 56]
+
+largest = lst[0]
+second_largest = lst[0]
+
+for num in lst:
+    if num > largest:
+        second_largest = largest
+        largest = num
+    elif num > second_largest and num != largest:
+        second_largest = num
+
+print("Second largest element is:", second_largest)
