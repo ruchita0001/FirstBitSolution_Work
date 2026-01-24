922c7ff138a8fa33dc37970597d05fd3457aa523
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-24 22:17:02 +0530

    Create que_8.py

---
+++ Core Python/Assignment/Assignment 10(List)/que_8.py
@@ -0,0 +1,10 @@
+#WAP to create a new list from existing list which contains cube of each number of list.
+
+lst = [1, 2, 3, 4]
+copy_list = []
+
+for i in lst:
+    copy_list.append(i)
+
+print("Original:", lst)
+print("Copy:", copy_list)
