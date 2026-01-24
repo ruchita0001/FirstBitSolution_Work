5aea88e5ede0e03d45f639d6f1ea958f8cdd3a6e
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-24 22:15:48 +0530

    Create que_7.py

---
+++ Core Python/Assignment/Assignment 10(List)/que_7.py
@@ -0,0 +1,10 @@
+#program to create a new list from existing list which contains cube of each number of list.
+
+li = [1, 2, 3, 4]
+cube = []
+
+for i in li:
+    cube.append(i * i * i)
+
+print(cube)
+
