4650ae72ddfbfef23158a3b39b37f748e1ff018e
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-27 00:41:50 +0530

    Update que_2.py

--- Core Python/Assignment/Assignment 4(looping)/que_2.py
+++ Core Python/Assignment/Assignment 4(looping)/que_2.py
@@ -1,6 +1,7 @@
 # WAP to print all odd numbers until n.
 
 n = int(input('Enter a number : '))
-for i in range(1, n+1):
-    if i % 2 != 0:
-        print(i, end=' ')
+i = 1
+while(i <= n):
+    print(i)
+    i += 2
