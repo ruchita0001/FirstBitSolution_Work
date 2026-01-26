caabddce9d43e20cbe7d07b73c298c856c02e7db
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:22:48 +0530

    Update que_4.py

--- Core Python/Assignment/Assignment 2(Basic)/que_4.py
+++ Core Python/Assignment/Assignment 2(Basic)/que_4.py
@@ -3,6 +3,8 @@
 length = int(input("Enter length: "))
 breadth = int(input("Enter breadth: "))
 
-area = length * breadth
+aor = length * breadth
+aot = 0.5 * length * breadth
 
-print("Area of rectangle =", area)
+print("Area of rectangle =", aor)
+print("Area of triangle =", aot)
