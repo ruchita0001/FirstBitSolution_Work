2cc48f965c1f381076771d02d3d6f465c2d039fb
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:29:36 +0530

    Update qu_10.py

--- Core Python/Assignment/Assignment 2(Basic)/qu_10.py
+++ Core Python/Assignment/Assignment 2(Basic)/qu_10.py
@@ -2,10 +2,9 @@
 
 num = int(input("Enter a three-digit number: "))
 
-a = num // 100        # first digit
-b = (num // 10) % 10  # middle digit
-c = num % 10          # last digit
+a = num // 100       
+b = (num // 10) % 10 
+c = num % 10         
 
 reverse = (c * 100) + (b * 10) + a
-
 print("Reversed number =", reverse)
