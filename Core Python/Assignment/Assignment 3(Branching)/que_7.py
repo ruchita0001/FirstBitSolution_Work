27067af42a5cfbced2eb3dd181cfce0d515faea7
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:34:44 +0530

    Update que_7.py

--- Core Python/Assignment/Assignment 3(Branching)/que_7.py
+++ Core Python/Assignment/Assignment 3(Branching)/que_7.py
@@ -1,15 +1,12 @@
 # WAP to check if user has entered correct userid and password.
 
-# Predefined correct credentials
-correct_userid = "admin"
-correct_password = "1234"
+C_userid = "Rucha"
+C_password = "1234"
 
-# Input from user
-userid = input("Enter UserID: ")
-password = input("Enter Password: ")
+userid = input("Enter UserID:")
+password = input("Enter Password:")
 
-# Check credentials
-if userid == correct_userid and password == correct_password:
-    print("Login successful")
+if userid == C_userid and password == C_password:
+    print("login successful")
 else:
     print("Invalid UserID or Password")
