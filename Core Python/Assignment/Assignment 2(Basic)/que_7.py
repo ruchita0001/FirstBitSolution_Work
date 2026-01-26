50f170cfed0d1192cbcee49d6d3c9a6edd74ef1c
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:27:45 +0530

    Update que_7.py

--- Core Python/Assignment/Assignment 2(Basic)/que_7.py
+++ Core Python/Assignment/Assignment 2(Basic)/que_7.py
@@ -2,10 +2,8 @@
 
 num = int(input("Enter a three-digit number: "))
 
-a = num // 100        # first digit
-b = (num // 10) % 10  # second digit
-c = num % 10          # third digit
-
+a = num // 100       
+b = (num // 10) % 10  
+c = num % 10          
 sum = a + b + c
-
 print("Sum of digits =", sum)
