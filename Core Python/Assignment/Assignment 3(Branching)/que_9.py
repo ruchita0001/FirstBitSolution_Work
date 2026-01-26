57a35820970ef2f357df930aba45d2cbda9ec790
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:38:03 +0530

    Update que_9.py

--- Core Python/Assignment/Assignment 3(Branching)/que_9.py
+++ Core Python/Assignment/Assignment 3(Branching)/que_9.py
@@ -1,15 +1,14 @@
-# Input marks for 5 subjects
-sub1 = int(input("Enter marks of subject 1: "))
-sub2 = int(input("Enter marks of subject 2: "))
-sub3 = int(input("Enter marks of subject 3: "))
-sub4 = int(input("Enter marks of subject 4: "))
-sub5 = int(input("Enter marks of subject 5: "))
+#Input 5 subject marks from user and display grade(eg.First class,Second class ..)
 
-# Calculate total and percentage
+sub1 = int(input("Enter marks of sub1: "))
+sub2 = int(input("Enter marks of sub2: "))
+sub3 = int(input("Enter marks of sub3: "))
+sub4 = int(input("Enter marks of sub4: "))
+sub5 = int(input("Enter marks of sub5: "))
+
 total = sub1 + sub2 + sub3 + sub4 + sub5
 percentage = (total / 500) * 100
 
-# Determine grade using branching
 if percentage >= 60:
     print("First Class")
 elif percentage >= 50:
