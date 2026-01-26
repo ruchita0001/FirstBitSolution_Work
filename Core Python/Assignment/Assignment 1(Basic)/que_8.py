22bfedc726ec14c966be77141aae7427495eee13
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:11:07 +0530

    Update que_8.py

--- Core Python/Assignment/Assignment 1(Basic)/que_8.py
+++ Core Python/Assignment/Assignment 1(Basic)/que_8.py
@@ -1,17 +1,13 @@
 # Write a program to convert days into years, weeks and days.
 
-# Input total days
-total_days = int(input("Enter the number of days: "))
+days = int(input("Enter the number of days: "))
 
-# Calculate years, weeks, and remaining days
-years = total_days // 365
-remaining_days_after_years = total_days % 365
+years = days // 365
+days = days % 365
 
-weeks = remaining_days_after_years // 7
-days = remaining_days_after_years % 7
+weeks = days // 7
+days = days % 7
 
-# Display the result
-print("Days:", int(total_days))
-print("Years:", int(years))
-print("Weeks:", int(weeks))
-print("Days:", int(days))
+print(f'Years = {years}')
+print(f'weeks = {weeks}')
+print(f'Days = {days}')
