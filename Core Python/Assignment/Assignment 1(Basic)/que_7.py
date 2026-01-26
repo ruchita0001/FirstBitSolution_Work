3e580fb6bec9e179c9bade16e7fe45074003081e
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:10:15 +0530

    Update que_7.py

--- Core Python/Assignment/Assignment 1(Basic)/que_7.py
+++ Core Python/Assignment/Assignment 1(Basic)/que_7.py
@@ -1,17 +1,13 @@
 # Program to Find the Roots of a Quadratic Equation
 
-# Input values
-a = float(input("Enter a: "))
-b = float(input("Enter b: "))
-c = float(input("Enter c: "))
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
 
-# Calculate discriminant
-d = b*b - 4*a*c
+D = b*b - 4*a*c  
 
-# Check conditions
-if d < 0:
-    print("Roots are imaginary")
-else:
-    x1 = (-b + d**0.5) / (2*a)
-    x2 = (-b - d**0.5) / (2*a)
-    print("Roots are:", x1, "and", x2)  
+r1 = (-b + D**0.5) / (2*a)
+r2 = (-b - D**0.5) / (2*a)
+
+print("Root 1 =", r1)
+print("Root 2 =", r2)
