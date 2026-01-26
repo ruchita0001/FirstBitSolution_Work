1cbf957601a7d222b60f6e8129b009391511ae0d
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:04:12 +0530

    Update que_1.py

--- Core Python/Assignment/Assignment 1(Basic)/que_1.py
+++ Core Python/Assignment/Assignment 1(Basic)/que_1.py
@@ -9,7 +9,4 @@ m5 = int(input("Enter marks for Subject 5: "))
 total_marks = m1 + m2 + m3 + m4 + m5 
 max_marks = 500
 percentage = (total_marks / max_marks) * 100
-
-print("total_Marks:", total_marks)
-print("max_marks:", max_marks)
-print("percentage:", int(percentage), "%")
+print(f'percentage : {percentage}%')
