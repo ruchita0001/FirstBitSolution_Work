bde982eb118c0c835a7d8eb816b622c32dad9687
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:40:38 +0530

    Update que_12.py

--- Core Python/Assignment/Assignment 3(Branching)/que_12.py
+++ Core Python/Assignment/Assignment 3(Branching)/que_12.py
@@ -1,14 +1,11 @@
 # WAP to check if given 3 digit number is a palindrome or not.
 
-# Input a three-digit number
 num = int(input("Enter a three-digit number: "))
 
-# Extract digits
-a = num // 100       # hundreds place
-b = (num // 10) % 10 # tens place
-c = num % 10         # units place
+a = num // 100       
+b = (num // 10) % 10 
+c = num % 10         
 
-# Check palindrome
 if a == c:
     print(num, "is a palindrome")
 else:
