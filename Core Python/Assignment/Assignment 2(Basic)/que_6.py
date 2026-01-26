9379c1fec5ffa12aa6f4791471a36e2e140f7fe4
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:26:43 +0530

    Update que_6.py

--- Core Python/Assignment/Assignment 2(Basic)/que_6.py
+++ Core Python/Assignment/Assignment 2(Basic)/que_6.py
@@ -1,10 +1,5 @@
 # WAP to calculate total salary of employee based on basic, da=10% of basic, ta=12% of basic, hra=15% of basic.
 
-# Given:
-# DA = 10% of basic
-# TA = 12% of basic
-# HRA = 15% of basic
-
 basic = int(input("Enter basic salary: "))
 
 da = (10 * basic) / 100
@@ -12,5 +7,4 @@ ta = (12 * basic) / 100
 hra = (15 * basic) / 100
 
 total_salary = basic + da + ta + hra
-
 print("Total Salary =", total_salary)
