87a9467107121e179e5e1f54a8efcc38c30ff802
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:14:45 +0530

    Update que_11.py

--- Core Python/Assignment/Assignment 1(Basic)/que_11.py
+++ Core Python/Assignment/Assignment 1(Basic)/que_11.py
@@ -1,17 +1,9 @@
 # Find the area and circumference of circle.
 
-# Input radius 
 radius = float(input("Enter the radius of the circle: "))
-
-# Value of pi
 pi = 3.14159
-
-# Calculate area
 area = pi * radius * radius
-
-# Calculate circumference
 circumference = 2 * pi * radius
 
-# Display the results
 print("Area of the circle:", area)
 print("Circumference of the circle:", circumference)
