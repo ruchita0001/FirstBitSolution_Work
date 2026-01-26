304844be4dcd0f4aa41f902208b46e1c9cc79c7d
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:18:33 +0530

    Update que_1.py

--- Core Python/Assignment/Assignment 2(Basic)/que_1.py
+++ Core Python/Assignment/Assignment 2(Basic)/que_1.py
@@ -1,1 +1,9 @@
-1
+# Convert the time entered in hh,min and sec into seconds.
+
+hours = int(input("Enter hours: "))
+minutes = int(input("Enter minutes: "))
+seconds = int(input("Enter seconds: "))
+
+total_seconds = (hours * 3600) + (minutes * 60) + seconds
+
+print("Total time in seconds:", total_seconds)
