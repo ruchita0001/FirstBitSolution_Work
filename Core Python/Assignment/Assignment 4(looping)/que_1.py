a9d39aa2ceeb486226772cd67584b768f454c86c
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-27 00:29:24 +0530

    Update que_1.py

--- Core Python/Assignment/Assignment 4(looping)/que_1.py
+++ Core Python/Assignment/Assignment 4(looping)/que_1.py
@@ -1,10 +1,8 @@
 # WAP to print all even numbers until n.
 
-# Input the value of n
 n = int(input('Enter a number: '))
 print('Even numbers up to', n, 'are:')
 
-# Loop from 1 to n
 for i in range(1, n+1):
-    if i % 2 == 0:  # Check if the number is divisible by 2
+    if i % 2 == 0:  
         print(i, end=' ')
