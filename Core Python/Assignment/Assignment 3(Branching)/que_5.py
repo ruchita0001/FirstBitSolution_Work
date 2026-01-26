715a695dfb2d2fefe128e26ff375737f8728dc7b
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-26 23:33:31 +0530

    Update que_5.py

--- Core Python/Assignment/Assignment 3(Branching)/que_5.py
+++ Core Python/Assignment/Assignment 3(Branching)/que_5.py
@@ -4,15 +4,9 @@ a = int(input("Enter first side: "))
 b = int(input("Enter second side: "))
 c = int(input("Enter third side: "))
 
-# First check if triangle is valid
-if (a + b > c) and (a + c > b) and (b + c > a):
-    
-    if a == b == c:
-        print("The triangle is equilateral")
-    elif a == b or b == c or a == c:
-        print("The triangle is isosceles")
-    else:
-        print("The triangle is scalene")
-        
+if a == b == c:
+    print("Triangle is Equilateral")
+elif a == b or b == c or a == c:
+    print("Triangle is Isosceles")
 else:
-    print("The triangle is not valid")
+    print("Triangle is Scalene")
