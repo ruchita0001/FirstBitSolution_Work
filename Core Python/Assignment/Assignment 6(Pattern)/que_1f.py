a2c8f5cb92b088f629123c1e3b9f3289ed70c282
Ruchita Gate <ruchitagate7@gmail.com>
2026-01-27 11:58:01 +0530

    Update que_1f.py

--- Core Python/Assignment/Assignment 6(Pattern)/que_1f.py
+++ Core Python/Assignment/Assignment 6(Pattern)/que_1f.py
@@ -6,13 +6,11 @@
 1 2 3 4 5 6 7 8 9
 
 
-n = 5   # number of rows
+n = 5  
 for i in range(1, n+1):
-    # printing spaces
     for space in range(n - i):
         print("  ", end="")
-
-    # printing numbers
     for num in range(1, 2*i):
         print(num, end=" ")
     print()
+
