557003179ca6b5a149d3b0af25a0b715c40c61b8
Ruchita Gate <ruchitagate7@gmail.com>
2026-02-02 20:38:16 +0530

    Update que_3b.py

--- Core Python/Assignment/Assignment 8(Functions)/que_3b.py
+++ Core Python/Assignment/Assignment 8(Functions)/que_3b.py
@@ -1,19 +1,17 @@
 # b. 1!+ 2! + 3! + 4!+..... + n!
 
-def sumOfFactorialSeries(n):
-    
-    for i in range(1,n+1):
+n = int(input("Enter value of n: "))
+
+def sum_fact(n):
+    for i in range(1, n+1):
         sum = 0
         fact = 1
-
-        for j in range(1,i+1):
+    
+        for j in range(1, i+1):
             fact *= j
             sum += fact
-
     return sum
 
-n = int(input("Enter the value of n : "))
-
-res = sumOfFactorialSeries(n)
+res = sum_fact(n)
 
 print(f"Sum of series of Factorial numbers upto {n} is {res}")
