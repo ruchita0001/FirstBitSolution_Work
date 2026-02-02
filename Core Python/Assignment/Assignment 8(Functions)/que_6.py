aec1acae05ea95f993ec82f6e62ec8b5ed22b42a
Ruchita Gate <ruchitagate7@gmail.com>
2026-02-02 20:52:56 +0530

    Update que_6.py

--- Core Python/Assignment/Assignment 8(Functions)/que_6.py
+++ Core Python/Assignment/Assignment 8(Functions)/que_6.py
@@ -1,17 +1,16 @@
 # WAP to find print the following Fibonacci series using functions:
 # 1 1 2 3 5 8 n terms
 
-def fibonacci(n, a, b):
-  
-    
-    for i in range(1, n+1):
-        c = a + b
+n = int(input("Enter a number: "))
+
+def fibonacci(n):
+    a = 1
+    b = 1
+    print(a, b, end=" ")
+    for i in range(3, n+1):
+        c = a+b
         print(c, end=" ")
+        a=b
+        b=c
 
-        a = b
-        b = c
-
-
-n = int(input("Enter the value of n: "))
-
-fibonacci(n,1,0)
+fibonacci(n)
