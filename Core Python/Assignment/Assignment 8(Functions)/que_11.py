cb9eb9d154a8768d81448d570780799fb59bf3e3
Ruchita Gate <ruchitagate7@gmail.com>
2026-02-02 20:48:49 +0530

    Update que_11.py

--- Core Python/Assignment/Assignment 8(Functions)/que_11.py
+++ Core Python/Assignment/Assignment 8(Functions)/que_11.py
@@ -1,40 +1,30 @@
 # WAP to check if a given number is Armstrong number or not. For each task create separate functions.
 
-# To count number of digits
-def count_digits(num):
+n = int(input("Enter number: "))
+
+def count_digits(n):
     count = 0
-    while num > 0:
-        count = count + 1
-        num = num // 10
+    while n > 0:
+        count += 1
+        n //= 10
     return count
 
-
-# To find power of a number
-def power(base, exp):
-    result = 1
-    for i in range(exp):
-        result = result * base
-    return result
-
-
-# To check Armstrong number
-def check_armstrong(num):
-    temp = num
-    digits = count_digits(num)
+def sum_pow(n, digits):
     total = 0
-
+    temp = n
     while temp > 0:
-        rem = temp % 10
-        total = total + power(rem, digits)
-        temp = temp // 10
+        digit = temp % 10
+        total += digit ** digits
+        temp //= 10
+    return total
 
-    if total == num:
-        print("Armstrong Number")
-    else:
-        print("Not an Armstrong Number")
-
-
-# Main program
-n = int(input("Enter a number: "))
-check_armstrong(n)
-
+def is_armstrong(n):
+    digits = count_digits(n)
+    result = sum_pow(n, digits)
+    return result == n
+    
+    
+if is_armstrong(n):
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
