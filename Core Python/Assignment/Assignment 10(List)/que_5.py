1ae974e280c9806524e44074798e9ae31b9b6101
Ruchita Gate <ruchitagate7@gmail.com>
2026-02-03 23:27:43 +0530

    Update que_5.py

--- Core Python/Assignment/Assignment 10(List)/que_5.py
+++ Core Python/Assignment/Assignment 10(List)/que_5.py
@@ -1,14 +1,14 @@
 #Accept a number from user and check if this element is present in the list or not. Also tell how many times it is present in the list.
 
-li = [1, 2, 3, 2, 4, 2]
-num = int(input())
+li = [10, 20, 30, 20, 40]
+num = int(input("Enter number: "))
 
 count = 0
 for i in li:
-    if i == num:
+    if (i == num):
         count += 1
 
-if count > 0:
-    print("Found", count, "times")
+if (count > 0):
+    print("Present", count, "times")
 else:
-    print("Not found")
+    print("Not Present")
