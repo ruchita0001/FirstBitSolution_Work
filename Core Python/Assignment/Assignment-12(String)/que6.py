a3aff666a159b1a0cc6fc8c869ad22b824f93339
Ruchita Gate <ruchitagate7@gmail.com>
2026-02-12 23:54:19 +0530

    Create que6.py

---
+++ Core Python/Assignment/Assignment-12(String)/que6.py
@@ -0,0 +1,7 @@
+# Take in a String and Replace Every Blank Space with Hyphen
+
+def replaceSpace(str1):
+    return str1.replace(' ', '-')
+
+str1 = input('Enter a string: ')
+print(replaceSpace(str1))
