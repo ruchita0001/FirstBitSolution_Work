ac26feb6e8c4f3c43cc821dd6258217b5ea3c300
Ruchita Gate <ruchitagate7@gmail.com>
2026-03-12 23:10:22 +0530

    Create Q1.py

---
+++ Core Python/Assignment/Assignment 14 (Set)/Q1.py
@@ -0,0 +1,7 @@
+# Write a Python program to find elements in a given set that are not in another set.
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {3, 4, 6}
+res = set1 - set2
+
+print("Elements in set1 but not in set2:", res)
