4fa3909187b9a83d5dc0d85d90fd5879013305ca
Ruchita Gate <ruchitagate7@gmail.com>
2026-03-23 23:32:50 +0530

    Implement student details display in main.py

---
+++ Core Python/Assignment/Assignment-17/Que_1/main.py
@@ -0,0 +1,11 @@
+from student import Student
+
+#Object using constructor
+s1 = Student()
+s1.accept()
+
+print('\n--- Student Details ---')
+s1.display()
+
+print('\n--- Using __str__ ---')
+print(s1)
