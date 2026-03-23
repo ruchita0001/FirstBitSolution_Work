ffab2b84fd762e3a50b6610b368fdc33dcec198d
Ruchita Gate <ruchitagate7@gmail.com>
2026-03-24 00:00:38 +0530

    Create Q7.py

---
+++ Core Python/Assignment/Assignment-19 (Comprehension, generator, decorator)/Comprehension/Q7.py
@@ -0,0 +1,6 @@
+#Use a nested list comprehension to find all of the numbers from 1–1000 that are divisible by any single digit.
+
+numbers = [i for i in range(1, 1001) 
+           if any(i % d == 0 for d in range(2, 10))]
+
+print(numbers)
