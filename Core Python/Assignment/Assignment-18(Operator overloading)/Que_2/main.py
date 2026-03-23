983695d6081f560e4cb49244f63dc2325d6d907e
Ruchita Gate <ruchitagate7@gmail.com>
2026-03-23 23:47:55 +0530

    Create main.py

---
+++ Core Python/Assignment/Assignment-18(Operator overloading)/Que_2/main.py
@@ -0,0 +1,14 @@
+from distance import Distance
+
+d1 = Distance(1, 500, 50)
+d2 = Distance(2, 600, 80)
+
+#Addition
+d3 = d1 + d2
+print('Addition:')
+d3.display()
+
+#Subtraction
+d4 = d1 - d2
+print('Subtraction:')
+d4.display()
