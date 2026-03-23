d7c9ed55904c1014a462a64ad9a2c0a88bfb58b7
Ruchita Gate <ruchitagate7@gmail.com>
2026-03-23 23:45:32 +0530

    Create main.py

---
+++ Core Python/Assignment/Assignment-18(Operator overloading)/Que_1/main.py
@@ -0,0 +1,14 @@
+from complex import Complex
+
+c1 = Complex(2, 3)
+c2 = Complex(1, 4)
+
+#Addition
+c3 = c1 + c2
+print('Addition:')
+c3.display()
+
+#Subtraction
+c4 = c1 - c2
+print('Subtraction:')
+c4.display()
